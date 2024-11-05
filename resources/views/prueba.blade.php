2f29891ca8a4ca94ba9c3ecaafd338cd1fd81d28
adriana <23610167@utgz.edu.mx>
2024-11-04 19:13:17 -0600

    Commit de Miproyecto

---
+++ resources/views/prueba.blade.php
@@ -0,0 +1,42 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>View Prueba</title>
+    <style>
+        body {
+            font-family: Arial, sans-serif;
+            background-color: #91adaa;
+            display: flex;
+            justify-content: center;
+            align-items: center;
+            flex-direction: column;
+            height: 100vh;
+            margin: 0;
+        }
+        h1 {
+            color: #333;
+            font-size: 2rem;
+            text-align: center;
+            padding: 20px;
+            border: 2px solid #c995b5;
+            background-color: #ffffff;
+            border-radius: 8px;
+            box-shadow: 0px 4px 8px rgba(0, 0, 0, 0.1);
+        }
+        img {
+            margin-top: 20px;
+            width: 200px; /* Ajusta el tamaño de la imagen */
+            height: auto;
+            border-radius: 8px;
+            box-shadow: 0px 4px 8px rgba(0, 0, 0, 0.2);
+        }
+    </style>
+</head>
+<body>
+    <h1>Primera prueba de vista en Laravel</h1>
+    <img src="/images/ejemplo.jfif" alt="Imagen de ejemplo"> <!-- Ruta de la imagen -->
+</body>
+</html>
